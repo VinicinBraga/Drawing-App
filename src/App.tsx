6ce1bdacc97d2877fabe2001370e7f658829a9ab
VinicinBraga <vinicius@bragamatta.com.br>
2023-05-11 16:33:40 -0300

    Css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 const App: React.FC = () => {
   return (
     <div className='app'>
-      <h1 className='title'>Blackboard Drawing</h1>
+      <h1 className='title'>PERSONAL BLACKBOARD</h1>
       <Blackboard />
     </div> 
   );
